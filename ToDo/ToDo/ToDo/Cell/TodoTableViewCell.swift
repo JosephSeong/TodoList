6e54cc35bfad39d5965712845c94e4b51b1be734
JosephSeong <joseph0903@naver.com>
2024-01-12 16:20:23 +0900

    modify switchChange

--- ToDo/ToDo/ToDo/Cell/TodoTableViewCell.swift
+++ ToDo/ToDo/ToDo/Cell/TodoTableViewCell.swift
@@ -9,20 +9,20 @@ import UIKit
 
 class TodoTableViewCell: UITableViewCell {
 
-
     @IBOutlet weak var doneSwitch: UISwitch!
 
     var todo: Todo?
+    var todoViewController: TodoViewController?
 
     // 왼료하면 밑줄
     @IBAction func switchChanged(_ sender: Any) {
         // todo 객체가 존재하는지 확인
-        guard var todo else { return }
+        guard var todo = self.todo else { return }
 
-        // todo 객체의 완료 상태를 갱신
+        // todo 객체의 완료 상태 업데이트
         todo.isCompleted = doneSwitch.isOn
 
-
+        // 완료 상태에 따라 셀의 텍스트에 밑줄을 추가하거나 제거
         if doneSwitch.isOn {
             textLabel?.text = nil
             textLabel?.attributedText = todo.title.strikeThrough()
@@ -30,6 +30,9 @@ class TodoTableViewCell: UITableViewCell {
             textLabel?.attributedText = nil
             textLabel?.text = todo.title
         }
+
+        // 업데이트된 데이터를 UserDefaults에 저장
+        todoViewController?.saveTodo()
 
         print(#function)
     }
