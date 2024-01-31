a429c1aeb3a3b5e8e955866db84d54ee60d41ae0
JosephSeong <joseph0903@naver.com>
2024-01-31 16:29:12 +0900

    refactoring MVVM

--- ToDo/ToDo/ToDo/ViewModel/ProfileViewModel.swift
+++ ToDo/ToDo/ToDo/ViewModel/ProfileViewModel.swift
@@ -11,17 +11,17 @@ class ProfileViewModel {
 
     var userProfile: UserProfile?
 
-    func fetchUserProfile(completion: @escaping () -> Void) {
-        let fetchedUserProfile = UserProfile(userName: "seong", userAge: 25)
-        self.userProfile = fetchedUserProfile
-        completion()
-    }
-
     var userName: String {
         return userProfile?.userName ?? ""
     }
 
     var userAge: String {
         return "\(userProfile?.userAge ?? 0)"
+    }
+
+    func fetchUserProfile(completion: @escaping () -> Void) {
+        let fetchedUserProfile = UserProfile(userName: "seong", userAge: 25)
+        self.userProfile = fetchedUserProfile
+        completion()
     }
 }
